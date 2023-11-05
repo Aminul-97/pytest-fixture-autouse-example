e63b3c60d6eb7c23bbf09cc235279bedd149a624
Aminul-97 <amin147570bd@gmail.com>
2023-11-05 22:28:22 +0600

    Update 05-11-2023

--- src/array_sorting.py
+++ src/array_sorting.py
@@ -1,58 +1,33 @@
-import sqlite3
-
-class sorting:
+class arr_sorting:
     def __init__(self, array: int) -> None:
         self.arr = array
     
     def insertion_sort(self):
        for i in range(1, len(self.arr)):
  
-        key = self.arr[i]
- 
-        j = i-1
-        while j >= 0 and key < self.arr[j] :
-                self.arr[j + 1] = self.arr[j]
-                j -= 1
-        self.arr[j + 1] = key
-        return self.arr
+            key = self.arr[i]
+    
+            j = i-1
+            while j >= 0 and key < self.arr[j] :
+                    self.arr[j + 1] = self.arr[j]
+                    j -= 1
+            self.arr[j + 1] = key
+        
+       return self.arr
 
-    def merge_sort(self):
-        if len(self.arr) > 1:
+    def selection_sort(self):
+        for ind in range(len(self.arr)):
+            min_index = ind
     
-            mid = len(self.arr)//2
-    
-            L = self.arr[:mid]
-    
-            R = self.arr[mid:]
-    
-            self.mergeSort(L)
-    
-            self.mergeSort(R)
-    
-            i = j = k = 0
-    
-            while i < len(L) and j < len(R):
-                if L[i] <= R[j]:
-                    self.arr[k] = L[i]
-                    i += 1
-                else:
-                    self.arr[k] = R[j]
-                    j += 1
-                k += 1
-    
-            # Checking if any element was left
-            while i < len(L):
-                self.arr[k] = L[i]
-                i += 1
-                k += 1
-    
-            while j < len(R):
-                self.arr[k] = R[j]
-                j += 1
-                k += 1 
+            for j in range(ind + 1, len(self.arr)):
+                # select the minimum element in every iteration
+                if self.arr[j] < self.arr[min_index]:
+                    min_index = j
+            # swapping the elements to sort the array
+            (self.arr[ind], self.arr[min_index]) = (self.arr[min_index], self.arr[ind])
         return self.arr
     
-    def bubbleSort(self):
+    def bubble_sort(self):
         n = len(self.arr)
         
         # Traverse through all array elements
@@ -69,11 +44,12 @@ class sorting:
 
         return self.arr
 
+"""
 sort_array = sorting([64, 34, 25, 12, 22, 11, 90])
-print(sort_array.insertion_sort())
-print(sort_array.merge_sort())
-print(sort_array.bubbleSort())
-
+print("Insertion sort: ", sort_array.insertion_sort())
+print("Selection sort: ", sort_array.selection_sort())
+print("Bubble sort: ", sort_array.bubbleSort())
+"""
 
 
 
--- test/test_array_sorting.py
+++ test/test_array_sorting.py
@@ -1,5 +1,33 @@
 import pytest
-from src.array_sorting import sorting
+from src.array_sorting import arr_sorting
 
 
+@pytest.fixture
+def set_array():
+    return []
 
+@pytest.fixture
+def initiate_class(set_array):
+    return arr_sorting(set_array)
+
+@pytest.fixture(autouse=True)
+def setup_test(set_array):
+    print("AUTOUSE fixture running")
+    set_array.append(64)
+    set_array.append(34)
+    set_array.append(25)
+    set_array.append(12)
+    set_array.append(22)
+    set_array.append(11)
+    set_array.append(90)
+
+def test_insertion_sort(initiate_class):
+    assert initiate_class.insertion_sort() == [11, 12, 22, 25, 34, 64, 90]
+
+
+def test_selection_sort(initiate_class):
+    assert initiate_class.selection_sort() == [11, 12, 22, 25, 34, 64, 90]
+
+
+def test_bubble_sort(initiate_class):
+    assert initiate_class.bubble_sort() == [11, 12, 22, 25, 34, 64, 90]
