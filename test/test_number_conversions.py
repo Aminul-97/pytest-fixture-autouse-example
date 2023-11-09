39cb58c2328017b8cbe1655ca8409c49a8ef3166
ericsalesdeandrade <sdaeric19@gmail.com>
2023-11-09 12:16:26 +0530

    Update test_number_conversions.py

--- test/test_number_conversions.py
+++ test/test_number_conversions.py
@@ -2,7 +2,7 @@ import pytest
 from src.number_conversions import NumberConversions
 
 
-# Define a fixture to yield a static number
+# Define a fixture to yield a static number with a `autouse=True` and session scope i.e the number is created once for all the tests
 @pytest.fixture(autouse=True, scope="session")
 def static_number():
     print("\nAUTOUSE fixture `static_number` setup...")
@@ -10,7 +10,7 @@ def static_number():
     print("\nAUTOUSE fixture `static_number` teardown...")
 
 
-# Define a fixture to create an instance of NumberConversions with a different scope
+# Define a fixture to create an instance of NumberConversions with a function scope i.e the instance is created for each test function
 @pytest.fixture(scope="function")
 def number_converter(static_number):
     return NumberConversions(static_number)
